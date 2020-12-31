a9b054cb28cde162e699fc4a017a9ca4c57b13b2
shlomp <shlomomatichin@gmail.com>
2020-12-31 09:54:52 +0200

    unittest: Moved common mocks to beforeAll

--- lib/terratag-action.spec.ts
+++ lib/terratag-action.spec.ts
@@ -11,61 +11,54 @@ const mockedTC = tc as jest.Mocked<typeof tc>;
 
 import run from './terratag-action';
 
-test('simple end to end, latest terratag', async () => {
-  mockedCore.getInput.mockImplementation((name: string) => {
-    const inputs: { [key: string]: string } = { terratagVersion: 'latest', tags: JSON.stringify({ a: 'b' }) };
-    return inputs[name];
+describe('terratag action', () => {
+  beforeEach(() => {
+    mockedTC.downloadTool.mockResolvedValue('FAKE PATH FOR DOWNLOADED TOOL TAR');
+    mockedTC.extractTar.mockResolvedValue('FAKE PATH FOR EXTRACTED');
+    const spawn = {
+      stdout: { on: jest.fn() },
+      stderr: { on: jest.fn() },
+      on: jest.fn()
+    };
+    spawn.on.mockImplementation((eventName: string, callback: (code: number) => void) => {
+      expect(eventName).toBe('close');
+      callback(0);
+    });
+    mockedChildProcess.spawn.mockReturnValue(spawn as any);
   });
-  mockedAxios.get.mockResolvedValue({
-    status: 200,
-    data: `href="/env0/terratag/releases/tag/v1.2.3"`
+  
+  test('simple end to end, latest terratag', async () => {
+    mockedCore.getInput.mockImplementation((name: string) => {
+      const inputs: { [key: string]: string } = { terratagVersion: 'latest', tags: JSON.stringify({ a: 'b' }) };
+      return inputs[name];
+    });
+    mockedAxios.get.mockResolvedValue({
+      status: 200,
+      data: `href="/env0/terratag/releases/tag/v1.2.3"`
+    });
+    await run();
+    expect(mockedCore.addPath.mock.calls).toEqual([['FAKE PATH FOR EXTRACTED']]);
+    expect(mockedAxios.get.mock.calls).toEqual([['https://github.com/env0/terratag/releases']]);
+    expect(mockedTC.downloadTool.mock.calls).toEqual([
+      ['https://github.com/env0/terratag/releases/download/v1.2.3/terratag_1.2.3_linux_amd64.tar.gz']
+    ]);
+    expect(mockedTC.extractTar.mock.calls).toEqual([['FAKE PATH FOR DOWNLOADED TOOL TAR']]);
+    expect(mockedChildProcess.spawn.mock.calls).toEqual([['FAKE PATH FOR EXTRACTED/terratag', ['-tags={"a":"b"}']]]);
   });
-  mockedTC.downloadTool.mockResolvedValue('FAKE PATH FOR DOWNLOADED TOOL TAR');
-  mockedTC.extractTar.mockResolvedValue('FAKE PATH FOR EXTRACTED');
-  const spawn = {
-    stdout: { on: jest.fn() },
-    stderr: { on: jest.fn() },
-    on: jest.fn()
-  };
-  spawn.on.mockImplementation((eventName: string, callback: (code: number) => void) => {
-    expect(eventName).toBe('close');
-    callback(0);
-  });
-  mockedChildProcess.spawn.mockReturnValue(spawn as any);
-  await run();
-  expect(mockedCore.addPath.mock.calls).toEqual([['FAKE PATH FOR EXTRACTED']]);
-  expect(mockedAxios.get.mock.calls).toEqual([['https://github.com/env0/terratag/releases']]);
-  expect(mockedTC.downloadTool.mock.calls).toEqual([
-    ['https://github.com/env0/terratag/releases/download/v1.2.3/terratag_1.2.3_linux_amd64.tar.gz']
-  ]);
-  expect(mockedTC.extractTar.mock.calls).toEqual([['FAKE PATH FOR DOWNLOADED TOOL TAR']]);
-  expect(mockedChildProcess.spawn.mock.calls).toEqual([['FAKE PATH FOR EXTRACTED/terratag', ['-tags={"a":"b"}']]]);
-});
 
-test('simple end to end, specific terratag', async () => {
-  mockedCore.getInput.mockImplementation((name: string) => {
-    const inputs: { [key: string]: string } = { terratagVersion: '5.6.7', tags: JSON.stringify({ a: 'b' }) };
-    return inputs[name];
+  test('simple end to end, specific terratag', async () => {
+    mockedCore.getInput.mockImplementation((name: string) => {
+      const inputs: { [key: string]: string } = { terratagVersion: '5.6.7', tags: JSON.stringify({ a: 'b' }) };
+      return inputs[name];
+    });
+    mockedAxios.get.mockRejectedValue('Should not be called');
+    await run();
+    expect(mockedCore.addPath.mock.calls).toEqual([['FAKE PATH FOR EXTRACTED']]);
+    expect(mockedAxios.get.mock.calls).toEqual([]);
+    expect(mockedTC.downloadTool.mock.calls).toEqual([
+      ['https://github.com/env0/terratag/releases/download/v5.6.7/terratag_5.6.7_linux_amd64.tar.gz']
+    ]);
+    expect(mockedTC.extractTar.mock.calls).toEqual([['FAKE PATH FOR DOWNLOADED TOOL TAR']]);
+    expect(mockedChildProcess.spawn.mock.calls).toEqual([['FAKE PATH FOR EXTRACTED/terratag', ['-tags={"a":"b"}']]]);
   });
-  mockedAxios.get.mockRejectedValue('Should not be called');
-  mockedTC.downloadTool.mockResolvedValue('FAKE PATH FOR DOWNLOADED TOOL TAR');
-  mockedTC.extractTar.mockResolvedValue('FAKE PATH FOR EXTRACTED');
-  const spawn = {
-    stdout: { on: jest.fn() },
-    stderr: { on: jest.fn() },
-    on: jest.fn()
-  };
-  spawn.on.mockImplementation((eventName: string, callback: (code: number) => void) => {
-    expect(eventName).toBe('close');
-    callback(0);
-  });
-  mockedChildProcess.spawn.mockReturnValue(spawn as any);
-  await run();
-  expect(mockedCore.addPath.mock.calls).toEqual([['FAKE PATH FOR EXTRACTED']]);
-  expect(mockedAxios.get.mock.calls).toEqual([]);
-  expect(mockedTC.downloadTool.mock.calls).toEqual([
-    ['https://github.com/env0/terratag/releases/download/v5.6.7/terratag_5.6.7_linux_amd64.tar.gz']
-  ]);
-  expect(mockedTC.extractTar.mock.calls).toEqual([['FAKE PATH FOR DOWNLOADED TOOL TAR']]);
-  expect(mockedChildProcess.spawn.mock.calls).toEqual([['FAKE PATH FOR EXTRACTED/terratag', ['-tags={"a":"b"}']]]);
 });
