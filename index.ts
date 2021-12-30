d359212e587a788fe2ab4ce769e61b7f43a85b21
eladmoshe <93523957+eladmosh@users.noreply.github.com>
2021-12-30 12:11:04 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import terratagAction from './lib/terratag-action';
   try {
     await terratagAction();
   } catch (error) {
-    console.info('blablabla');
+    core.info('blablabla');
     console.error(`Action JS error: ${error}`);
     core.setFailed(error.message);
   }
