8ec93598335ef3ab113c38f91a8927f89b143140
eladmoshe <elad.moshe@env0.com>
2022-01-02 18:19:00 +0200

    fixing tests

--- lib/terratag-action.spec.ts
+++ lib/terratag-action.spec.ts
@@ -1,9 +1,12 @@
+import os from 'os';
 import axios from 'axios';
 import childProcess from 'child_process';
-import core from '@actions/core';
-import tc from '@actions/tool-cache';
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
 jest.mock('axios');
 jest.mock('child_process');
+jest.mock('@actions/core');
+jest.mock('@actions/tool-cache');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 const mockedChildProcess = childProcess as jest.Mocked<typeof childProcess>;
 const mockedCore = core as jest.Mocked<typeof core>;
@@ -12,6 +15,7 @@ const mockedTC = tc as jest.Mocked<typeof tc>;
 import run from './terratag-action';
 
 describe('terratag action', () => {
+  const osPlatform = os.platform();
   beforeEach(() => {
     mockedTC.downloadTool.mockResolvedValue('FAKE PATH FOR DOWNLOADED TOOL TAR');
     mockedTC.extractTar.mockResolvedValue('FAKE PATH FOR EXTRACTED');
@@ -44,7 +48,7 @@ describe('terratag action', () => {
     });
     it('should download terratag from expected url', () => {
       expect(mockedTC.downloadTool.mock.calls).toEqual([
-        ['https://github.com/env0/terratag/releases/download/v1.2.3/terratag_1.2.3_linux_amd64.tar.gz']
+        [`https://github.com/env0/terratag/releases/download/v1.2.3/terratag_1.2.3_${osPlatform}_amd64.tar.gz`]
       ]);
     });
     it('should extract downloaded terratag', () => {
@@ -72,7 +76,7 @@ describe('terratag action', () => {
     });
     it('should download terratag from expected url', () => {
       expect(mockedTC.downloadTool.mock.calls).toEqual([
-        ['https://github.com/env0/terratag/releases/download/v5.6.7/terratag_5.6.7_linux_amd64.tar.gz']
+        [`https://github.com/env0/terratag/releases/download/v5.6.7/terratag_5.6.7_${osPlatform}_amd64.tar.gz`]
       ]);
     });
     it('should extract downloaded terratag', () => {
